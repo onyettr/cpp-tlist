3d0b02a7e1ec7710981afa389b1c83871feb5e5b
Richard Onyett <ronyett@megachips.com>
2018-11-29 08:54:43 -0800

    formatting changes

--- list.h
+++ list.h
@@ -79,31 +79,31 @@ class linked_list {
         linked_list(int number, const T&);/*! linked list fill constructor n elements, assigned T */
         ~linked_list(void);               /*! linked list destructor       */  
 
-    // Member functions
-    void list_assign      (int n, const T& value);        // Assign (fill) n elemnts to value
-    void list_add_element (const T& value);               // Add an element to the list, increases list size by 1
-    void list_add_at_front(const T& value);               // Add an element to the front of the list, increases list size by 1
-    void list_add_at_back (const T& value);               // Add an element to the back  of the list, increases list size by 1
-    void list_add_position(int position, const T& value); // Add an element at position,increases list size by 1
-    T    list_get_position(int position);                 // return value at "position"
-    T    list_get_front   (void);                         // return value of first list element
-    T    list_get_back    (void);                         // return value of tail list element
-    void list_delete_element(int position);               // Delete element  at position, decreases list size by 1
-    void list_delete_front(void);                         // Delete first element of list, decreases list size by 1
-    void list_delete_back (void);                         // Delete last  element of list, decreases list size by 1
-    void list_clear       (void);                         // Delete all elements of list, decreases list size to 0
-    void list_reverse     (void);                         // Reverse the contents of list
-    int  list_search      (const T& value);               // Search list for value, return position
-    void list_sort        (void);                         // Sort the list
-    void list_show        (void);                         // Show the contents of the linked list
-    int  list_size        (void);                         // Number of elements in the list
-    bool list_empty       (void);                         // is the list empty?
-    list_element_t *GetListHead(void) {                   // Obtain Head iterator pointing to first element
-        return pHead;
-    }
-    list_element_t *GetListTail(void) {                   // Obtain Tail iterator pointing to last element
-        return pTail;
-    }
+        // Member functions
+        void list_assign      (int n, const T& value);        // Assign (fill) n elemnts to value
+        void list_add_element (const T& value);               // Add an element to the list, increases list size by 1
+        void list_add_at_front(const T& value);               // Add an element to the front of the list, increases list size by 1
+        void list_add_at_back (const T& value);               // Add an element to the back  of the list, increases list size by 1
+        void list_add_position(int position, const T& value); // Add an element at position,increases list size by 1
+        T    list_get_position(int position);                 // return value at "position"
+        T    list_get_front   (void);                         // return value of first list element
+        T    list_get_back    (void);                         // return value of tail list element
+        void list_delete_element(int position);               // Delete element  at position, decreases list size by 1
+        void list_delete_front(void);                         // Delete first element of list, decreases list size by 1
+        void list_delete_back (void);                         // Delete last  element of list, decreases list size by 1
+        void list_clear       (void);                         // Delete all  elements of list, decreases list size to 0
+        void list_reverse     (void);                         // Reverse the contents of list
+        int  list_search      (const T& value);               // Search list for value, return position
+        void list_sort        (void);                         // Sort the list
+        void list_show        (void);                         // Show the contents of the linked list
+        int  list_size        (void);                         // Number of elements in the list
+        bool list_empty       (void);                         // is the list empty?
+        list_element_t *GetListHead(void) {                   // Obtain Head iterator pointing to first element
+        	return pHead;
+        }
+        list_element_t *GetListTail(void) {                   // Obtain Tail iterator pointing to last element
+        	return pTail;
+        }
 };
 
 #include "list.cpp"
